14e33961346d36fe2d605e4dbc9b8db91bd9cf18
Quentin Fonck <quentin.fonck@internet.lu>
2016-12-06 14:11:29 +0100

    Ajout de postCss au projet

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss')
+autoprefixer = require('autoprefixer');
 
 gulp.task('default', function(){
   console.log('Ceci est un test gulp');
@@ -9,14 +11,18 @@ gulp.task('html', function() {
   console.log('test sur le html');
 });
 gulp.task('styles', function() {
-  console.log('On fait du css!!!');
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
+
   watch('./app/index.html', function(){
     gulp.start('html');
   });
   watch('./app/assets/**/*.css', function(){
     gulp.start('styles');
   });
+
 });

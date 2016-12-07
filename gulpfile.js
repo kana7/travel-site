f151e2a0df6bc85202ce610c7f3f2aa33cf5ef22
quentin Fonck <quentin.fonck@gmail.com>
2016-12-07 23:02:54 +0100

    Ajout de postcss-import

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,31 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss')
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-cssnested = require('postcss-nested');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    cssnested = require('postcss-nested'),
+    cssimport = require('postcss-import');
 
-gulp.task('default', function(){
-  console.log('Ceci est un test gulp');
+gulp.task('default', function() {
+    console.log('Ceci est un test gulp');
 });
 
 gulp.task('html', function() {
-  console.log('test sur le html');
+    console.log('test sur le html');
 });
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssnested, cssvars, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, cssnested, cssvars, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
-  watch('./app/assets/**/*.css', function(){
-    gulp.start('styles');
-  });
+    watch('./app/index.html', function() {
+        gulp.start('html');
+    });
+    watch('./app/assets/**/*.css', function() {
+        gulp.start('styles');
+    });
 
 });

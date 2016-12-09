fef97541afb041eef971db8f9da40707ebc504c3
Quentin Fonck <quentin.fonck@internet.lu>
2016-12-09 14:19:43 +0100

    ajout postcss-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,15 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     cssnested = require('postcss-nested'),
-    cssimport = require('postcss-import');
+    cssimport = require('postcss-import'),
+    cssmixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, cssnested, cssvars, autoprefixer]))
+        .pipe(postcss([cssimport, cssmixins, cssnested, cssvars, autoprefixer]))
+        .on('error', function(errorInfo){
+          console.log(errorInfo.toString());
+          this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 });

e0a59c9f7a56389542e5de093bd779e6d59baa8d
Quentin Fonck <quentin.fonck@internet.lu>
2016-12-15 14:23:57 +0100

    debut de la msise en page du header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars'),
-    cssnested = require('postcss-nested'),
-    cssimport = require('postcss-import'),
-    cssmixins = require('postcss-mixins');
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+cssnested = require('postcss-nested'),
+cssimport = require('postcss-import'),
+cssmixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
-    return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, cssmixins, cssnested, cssvars, autoprefixer]))
-        .on('error', function(errorInfo){
-          console.log(errorInfo.toString());
-          this.emit('end');
-        })
-        .pipe(gulp.dest('./app/temp/styles'));
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssimport, cssmixins, cssnested, cssvars,hexrgba, autoprefixer]))
+  .on('error', function(errorInfo){
+    console.log(errorInfo.toString());
+    this.emit('end');
+  })
+  .pipe(gulp.dest('./app/temp/styles'));
 });
